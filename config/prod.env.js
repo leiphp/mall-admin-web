fdfd1f045c30a71f194e86240d78a6b556026c47
leiwen <leiwen@jmfen.cn>
2021-02-01 17:07:35 +0800

    feat：切换网关域名

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,5 +2,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   // BASE_API: '"http://admin-api.macrozheng.com"'
-  BASE_API: '"http://xx.xx.xx.xx:8201/mall-admin"'
+  BASE_API: '"http://gw.100txy.com/mall-admin"'
 }
